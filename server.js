9d5c002e30012d66ec6bb3a6dc2ec408727ce7ed
NandhinyPandurangan <nandhinyp95@gmail.com>
2017-02-02 03:53:05 -0800

    [imad-console] Updates server.js

--- server.js
+++ server.js
@@ -8,11 +8,11 @@ app.use(morgan('combined'));
 app.get('/', function (req, res) {
   res.sendFile(path.join(__dirname, 'ui', 'index.html'));
 });
-app.get('/articleone',function(req,res)
+app.get('/article-one',function(req,res)
 {res.sendFile("article one is requested")});
-app.get('/articletwo',function(req,res)
+app.get('/article-two',function(req,res)
 {res.sendFile("article two is requested")});
-app.get('/articlethree',function(req,res)
+app.get('/article-three',function(req,res)
 {res.sendFile("article three is requested")});
 
 
