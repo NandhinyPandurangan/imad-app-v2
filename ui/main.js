43f1043d1e13e69a53d02fb35765f36fe6a39a3c
NandhinyPandurangan <nandhinyp95@gmail.com>
2017-02-03 01:31:19 -0800

    [imad-console] Updates ui/main.js

--- ui/main.js
+++ ui/main.js
@@ -1,3 +1,9 @@
 console.log('Loaded!');
 var element=document.getElementById('main text');
 element.innerHTML='Nandhiny imad app';
+var img=document.getElementById('madi');
+img.onclick=function()
+{
+    img.stylemarginLeft='100px';
+    
+}
