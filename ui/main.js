ee9704564bb743e4f4ee40db81a52bc4b5d6f327
NandhinyPandurangan <nandhinyp95@gmail.com>
2017-02-03 04:04:00 -0800

    [imad-console] Updates ui/main.js

--- ui/main.js
+++ ui/main.js
@@ -19,3 +19,16 @@ button.onclick=function()
         request.send(null);
     
     };  
+    var nameInput=document.getElementById('name');
+    var name=nameInput.value;
+    var submit=document.getElementById('submit');
+    submit.onclick=function(){
+        var names=["name1","name2","name3","name4"];
+        var list="";
+        for(vari=0;i<names.length;i++)
+        {
+            list='<li>'+names[i]+'</li>';
+        }
+        var ul=documen.getElementById('namelist');
+        ul.innerHTML=list;
+    }
